17a417af5fd99284a33636b63578910076a91ace
Ernest Fiifi Des-Bordes <ernestdesbordes@gmail.com>
2025-09-10 16:52:53 +0000

    feat: integrate Lufga font family and update styles for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { lufga } from "@/lib/fonts";
 import { Analytics } from "@vercel/analytics/next";
 import { Suspense } from "react";
 import "./globals.css";
@@ -19,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`font-sans overflow-x-hidden ${GeistSans.variable} ${GeistMono.variable}`}
-      >
+      <body className={`font-sans overflow-x-hidden ${lufga.variable}`}>
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
